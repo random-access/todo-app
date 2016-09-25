f3e4e81933feeb80b9b8dcead29c6848abfe6b54
moni@random-access.org <moni@random-access.org>
2016-09-25 14:46:38 +0000

    Updated DB script; fixed navigation to addTask

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,13 +1,15 @@
+-- Delete all tables from database, if the exist
 DROP table if exists tasks;
 DROP table if exists projects;
 
+-- Create projects table
 CREATE TABLE projects (
 	id int(11) NOT NULL AUTO_INCREMENT,
     name varchar(255) DEFAULT NULL,
     PRIMARY KEY(id)
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+-- Create tasks table
 CREATE TABLE tasks (
 	id int(11) NOT NULL AUTO_INCREMENT,
     name varchar(255) DEFAULT NULL,
@@ -18,15 +20,17 @@ CREATE TABLE tasks (
         REFERENCES projects(id)
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO todo.projects (name) VALUES ("Privat");
-INSERT INTO todo.projects (name) VALUES ("Uni");
-INSERT INTO todo.projects (name) VALUES ("Arbeit");
+-- Add some records to projects table
+INSERT INTO todo.projects (name) VALUES ("Projekt 1");
+INSERT INTO todo.projects (name) VALUES ("Projekt 2");
+INSERT INTO todo.projects (name) VALUES ("Projekt 3");
 
-INSERT INTO todo.tasks (name, description, project) VALUES ("Einkaufen", "Brot, Milch, Käse, Salat, Schokolade", 1);
-INSERT INTO todo.tasks (name, description, project) VALUES ("1657 EA1", "Bis 15.10.2015 fertig", 2);
-INSERT INTO todo.tasks (name, description, project) VALUES ("Java EE", "Herausfinden welche Technologien ich lernen muss", 3);
-INSERT INTO todo.tasks (name, description, project) VALUES ("Hibernate", "Tutorial finden", 3);
-INSERT INTO todo.tasks (name, description, project) VALUES ("1896 KE1", "Durchlesen und Zusammenfassung", 2);
-INSERT INTO todo.tasks (name, description, project) VALUES ("Regal", "Regal aussuchen und einkaufen", 1);
-INSERT INTO todo.tasks (name, description, project) VALUES ("JPA", "Herausfinden wie n:m Beziehungen funktionieren", 3);
-INSERT INTO todo.tasks (name, description, project) VALUES ("Linux", "Bash-Scripting weiter lernen", 1);
+-- Add some records to tasks table
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 1", "Beschreibung von Task 1", 1);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 2", "Beschreibung von Task 2", 2);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 3", "Beschreibung von Task 3", 3);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 4", "Beschreibung von Task 4", 3);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 5", "Beschreibung von Task 5", 2);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 6", "Beschreibung von Task 6", 1);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 7", "Beschreibung von Task 7", 3);
+INSERT INTO todo.tasks (name, description, project) VALUES ("Task 8", "Beschreibung von Task 8", 1);
--- db/seed_projects.sql
+++
@@ -1,5 +0,0 @@
-delete from todo.projects;
-INSERT INTO todo.projects (name) VALUES ("Privat");
-INSERT INTO todo.projects (name) VALUES ("Uni");
-INSERT INTO todo.projects (name) VALUES ("Arbeit");
-
